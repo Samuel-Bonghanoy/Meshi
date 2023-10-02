d558f5ebd2dddd828de0e3ed2cd79e14182a7340
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-02 23:45:03 +0800

    feat: implemented geocoding

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -27,16 +27,18 @@ function MenuItem({ pizza }) {
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''} rounded`}
       />
       <div className="flex grow flex-col pt-0.5">
-        <p className="font-medium">{name}</p>
-        <p className="text-sm capitalize italic text-stone-500">
+        <p className="font-medium text-stone-200">{name}</p>
+        <p className="text-sm capitalize italic text-stone-200">
           {ingredients.join(', ')}
         </p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
-            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+            <p className="text-sm text-yellow-400">
+              {formatCurrency(unitPrice)}
+            </p>
           ) : (
             <p className="text-sm font-medium uppercase text-stone-500">
               Sold out
